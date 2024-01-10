361621f1112c7427e7abefe252795762dd63f8a2
jacquelinesantana <ti.jacque@gmail.com>
2024-01-10 00:55:34 -0300

    banco de dados ajustado deploy 3

--- blogpessoal/src/main/java/com/generation/blogpessoal/security/UserDetailsServiceImpl.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/security/UserDetailsServiceImpl.java
@@ -1,5 +1,5 @@
 package com.generation.blogpessoal.security;
-
+ 
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- blogpessoal/src/main/java/com/generation/blogpessoal/service/UsuarioService.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/service/UsuarioService.java
@@ -25,7 +25,7 @@ public class UsuarioService {
 	@Autowired
     private JwtService jwtService;
 
-    @Autowired
+    @Autowired 
     private AuthenticationManager authenticationManager;
 
 	public Optional<Usuario> cadastrarUsuario(Usuario usuario) {
